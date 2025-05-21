79c75998a40b09b772d274b5c22ee57de368ba1a
Anvesh Khode <anveshkhode7588@gmail.com>
2025-05-21 12:09:59 +0530

    Initial setup

---
+++ frontend/src/lib/components/shared/Footer.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<div class="text-center">
+<p> Footer </p>
+<ul>List
+    <li>Item 1</li>
+    <li>Item 2</li>
+    <li>Item 3</li>
+    <li>Item 4</li>
+    <li>Item 5</li>
+</ul>
+</div>
---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import Header from '../lib/components/shared/Header.svelte'
+    import Footer from '../lib/components/shared/Footer.svelte'
+</script>
+
+<Header />
+
+<h1>
+    Notenex - A note sharing Platform for my college friends
+</h1>
+
+<Footer />
